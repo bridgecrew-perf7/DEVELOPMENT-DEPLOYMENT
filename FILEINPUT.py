797334388d280fc552ff1bbca4e1f74d7258ab11
Dennis Burkhard <public@hire-js.com>
2021-05-27 01:50:45 +0200

    Add workable solutions

--- FILEINPUT.py
+++ FILEINPUT.py
@@ -3,10 +3,10 @@ import fileinput
 import csv
 import re
 
-in_filename = '/home/mac/Desktop/PD-LA/MM.csv'
-out_filename = '/home/mac/Desktop/PD-LA/MMout.csv'
+in_filename = '/home/mac/Desktop/PD-LA/WW.csv'
+out_filename = '/home/mac/Desktop/PD-LA/WWout.csv'
 
 with open(in_filename) as infile, open(out_filename, "w") as outfile:
     for line in infile.readlines():
-        line = re.sub(r"(\n)|(\s)|(W)", "", line)
+        line = re.sub(r";;", ";", line)
         outfile.write(line)
--- STOP-WORDS.py
+++ STOP-WORDS.py
@@ -1,34 +1,23 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-import umap
 import csv
 import nltk
-import sklearn
-
-df = pd.read_csv('/home/mac/Desktop/PD-LA/PDTINKERTEXT.csv', header=None, engine='python')
-
-df = df.str.replace("[^a-zA-Z#]", ";")
-
-df = df.apply(lambda x: ' '.join([w for w in x.split() if len(w)>3]))
-
-df = df.apply(lambda x: x.lower())
 
 from nltk.corpus import stopwords
 stop_words = stopwords.words('english')
 
-# tokenization
-tokenized_doc = news_df['clean_doc'].apply(lambda x: x.split())
+df = pd.read_csv('/home/mac/Desktop/PD-LA/PDTINKERTEXT.csv', header=None, engine='python')
 
-# remove stop-words
+tokenized_doc = df.apply(lambda x: x.split())
+
 tokenized_doc = tokenized_doc.apply(lambda x: [item for item in x if item not in stop_words])
 
-# de-tokenization
 detokenized_doc = []
-for i in range(len(news_df)):
+for i in range(len(df)):
     t = ' '.join(tokenized_doc[i])
     detokenized_doc.append(t)
 
-news_df['clean_doc'] = detokenized_doc
+df = detokenized_doc
+
+print(df)
