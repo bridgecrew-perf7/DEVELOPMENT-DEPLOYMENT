d23f27c1809eee0994cf9f7476e742d82102c250
Dennis Burkhard <public@hire-js.com>
2021-05-26 23:50:12 +0200

    Pandas Setup Linear Algebra

---
+++ PDTINKER.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import umap
+import csv
+import nltk
+import sklearn
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
+
+pd.set_option("display.max_colwidth", 200)
+
+#all lowercase, only alphabetical characters, separator is;
+df = pd.read_csv('/home/mac/Desktop/PD-LA/PDTINKERTEXT.csv', header=None, engine='python')
+df = df[0].str.split(';', expand=True)
+#df = df.to_csv("Test.tab",sep=";", header=False, index=False)
+
+
+
+print(df)
