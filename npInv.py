fa1f5efcb5edab4edb7fc7ff2454303827436c9e
Dennis Burkhard <public@hire-js.com>
2021-06-04 22:58:14 +0200

    Inverse matrix

---
+++ npInv.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+a = np.matrix('1 2 7; 3 4 12; 2 5 1')
+
+# def matrix_cofactor(matrix):
+#    return np.linalg.inv(matrix).T * np.linalg.det(matrix)
+
+#print(np.linalg.inv(a))
+
+#print(np.linalg.det(a))
