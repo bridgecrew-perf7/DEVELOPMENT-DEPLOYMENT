26d60fd9ed1e1cc1af3c65782a117c4660eeb912
Dennis Burkhard <public@hire-js.com>
2021-07-11 01:33:56 +0200

    Imrpove word freq

--- DATA_VIZ_WORD_FREQ_PYTHON.py
+++ DATA_VIZ_WORD_FREQ_PYTHON.py
@@ -3,27 +3,19 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 import csv
-import nltk
-import sklearn
 import sys
-import PIL
 
-#nltk.download('stopwords')
 csv.field_size_limit(sys.maxsize)
-
-from nltk.corpus import stopwords
-stop = stopwords.words('english')
 
 pd.set_option("display.max_colwidth", 500)
 
-#get the df
+#get the 1-dim df
 df = pd.read_csv('/home/mac/Desktop/PANDAS-ML/IN.csv', header=None, engine='python')
 
-#count and visualize
+#split and count
 col = df[0].str.split(';', expand=True).stack().value_counts(normalize=True)
 
-col.plot(kind="bar").set_xlim(0.01500, 0.0004)
+col.plot(kind="bar")
 
 plt.show()
 #print(df)
-#print(col)
