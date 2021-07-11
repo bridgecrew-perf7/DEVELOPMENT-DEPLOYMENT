a3852d0820d81a0af6e3b0fcd666aabb087be020
Dennis Burkhard <public@hire-js.com>
2021-07-11 02:18:41 +0200

    Add word freq by tokenizer

--- DATA_VIZ_WORD_FREQ_PYTHON.py
+++ DATA_VIZ_WORD_FREQ_PYTHON.py
@@ -4,18 +4,16 @@ import pandas as pd
 
 import csv
 import sys
+import PIL
 
 csv.field_size_limit(sys.maxsize)
 
 pd.set_option("display.max_colwidth", 500)
 
-#get the 1-dim df
 df = pd.read_csv('/home/mac/Desktop/PANDAS-ML/IN.csv', header=None, engine='python')
 
-#split and count
 col = df[0].str.split(';', expand=True).stack().value_counts(normalize=True)
 
 col.plot(kind="bar")
 
 plt.show()
-#print(df)
